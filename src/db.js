90ca396a15aa61510b9ed2314b2588f3e3cb7db5
Vidombax <kirilka360@gmail.com>
2024-12-17 11:55:25 +0500

    ssl проверка

--- src/db.js
+++ src/db.js
@@ -8,5 +8,8 @@ const db = new pg.Pool({
     host: `${process.env.POSTGRES_HOST}`,
     database: `${process.env.POSTGRES_DATABASE}`,
     port: `${process.env.POSTGRES_PORT}`,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 })
 export default db;
